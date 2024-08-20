201c7299dd0c3856ccdac323c83e5a3988a0d48a
Wahyu Nur Anggoro Wati <156877081+wahyunur3102@users.noreply.github.com>
2024-08-20 09:10:02 +0700

    Add files via upload

---
+++ 2 PROJECT MICRODOT/boot.py
@@ -0,0 +1,28 @@
+# boot.py -- run on boot-up
+import network, utime
+
+# Replace the following with your WIFI Credentials
+SSID = "BOE-"
+SSI_PASSWORD = ""
+
+def do_connect():
+    import network
+    sta_if = network.WLAN(network.STA_IF)
+    if not sta_if.isconnected():
+        print('connecting to network...')
+        sta_if.active(True)
+        sta_if.connect(SSID, SSI_PASSWORD)
+        while not sta_if.isconnected():
+            pass
+    print('Connected! Network config:', sta_if.ifconfig())
+    
+    ip= sta_if.ifconfig()[0]
+    print(ip)
+    return ip
+
+print("Connecting to your wifi..." '')
+do_connect()
+
+print(do_connect())
+
+
